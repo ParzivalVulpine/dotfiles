887bafd2373f3567de2216e9230a14eabf798528
ParzivalVulpine <dgaitan@calicofox.dev>
2025-02-08 22:24:08 -0500

    Fix fish config directory structure

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,13 +11,12 @@ end
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /Users/parzv/miniconda3/bin/conda
-    eval /Users/parzv/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    eval /Users/parzv/miniconda3/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/Users/parzv/miniconda3/etc/fish/conf.d/conda.fish"
         . "/Users/parzv/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/Users/parzv/miniconda3/bin" $PATH
+        set -x PATH /Users/parzv/miniconda3/bin $PATH
     end
 end
 # <<< conda initialize <<<
-
--- fish/.config/functions/ll.fish
+++
@@ -1,4 +0,0 @@
-function ll --wraps=ls --wraps='ls -la' --description 'alias ll=ls -la'
-  ls -la $argv
-        
-end
