7a2e37127e90e39a81a277dc3035b8ba86633b7e
ParzivalVulpine <dgaitan@calicofox.dev>
2025-02-11 17:47:26 -0500

    Add obsidian vault to launch fish fucntion

--- fish/.config/fish/functions/launch.fish
+++ fish/.config/fish/functions/launch.fish
@@ -2,7 +2,7 @@ function launch
     if test (count $argv) -eq 1
         set selected $argv[1]
     else
-        set selected (find ~/Projects ~/Projects/datil -mindepth 1 -maxdepth 1 -type d | fzf)
+        set selected (find ~/Projects ~/Projects/datil ~/Documents/obsidian -mindepth 1 -maxdepth 1 -type d | fzf)
     end
 
     if test -z "$selected"
