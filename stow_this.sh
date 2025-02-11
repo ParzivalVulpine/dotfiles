8307710110c72bee8c1afacbc62178fe612e71d4
ParzivalVulpine <dgaitan@calicofox.dev>
2025-02-11 17:53:19 -0500

    Fix launch script string escaping

--- stow_this.sh
+++ stow_this.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create an array of folder names
-folders=("nvim" "fish" "starship" "btop" "gh" "ghostty" "godot" "thefuck" "tmux" "launcher")
+folders=("nvim" "fish" "starship" "btop" "gh" "ghostty" "godot" "thefuck" "tmux")
 
 # Loop through each folder in the array
 for folder in "${folders[@]}"; do
   echo "stowing $folder"
-  stow -D $folder
-  stow $folder
+  stow -D "$folder"
+  stow "$folder"
 done
