1484918504106a705bcfcb77654e8cf9ad3b0da9
ParzivalVulpine <dgaitan@calicofox.dev>
2025-02-11 11:28:11 -0500

    Move launch to fish as a function

---
+++ fish/.config/fish/functions/launch.fish
@@ -0,0 +1,14 @@
+function launch
+    if test (count $argv) -eq 1
+        set selected $argv[1]
+    else
+        set selected (find ~/Projects ~/Projects/datil -mindepth 1 -maxdepth 1 -type d | fzf)
+    end
+
+    if test -z "$selected"
+        return 0
+    end
+
+    cd "$selected"
+    nvim .
+end
