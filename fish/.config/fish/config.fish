7a62eb4ad011a09a1173f6e347c2a80c0d861c97
ParzivalVulpine <dgaitan@calicofox.dev>
2025-05-21 12:17:27 -0500

    chore(fish): clean up config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -108,20 +108,3 @@ end
 function fish_greeting
     fastfetch
 end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/parzv/miniconda3/bin/conda
-    eval /Users/parzv/miniconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/Users/parzv/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/Users/parzv/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /Users/parzv/miniconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
