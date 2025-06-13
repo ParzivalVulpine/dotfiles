384f4f01cb5a971e2a6c96183cf26c771db8deb7
ParzivalVulpine <dgaitan@calicofox.dev>
2025-06-13 10:40:18 -0500

    feat(nvim): update godot lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,7 +4,18 @@ return {
     version = "1.11.0",
     opts = {
       servers = {
-        gdscript = {},
+        gdscript = {
+          cmd = { "nc", "127.0.0.1", "6005" },
+          filetypes = { "gd", "gdscript", "gdscript3" },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "project.godot",
+              ".git"
+            )(fname)
+          end,
+          single_file_support = false,
+          settings = {},
+        },
         gdshader_lsp = {},
         ruff = {
           cmd_env = { RUFF_TRACE = "messages" },
@@ -17,13 +28,31 @@ return {
         templ = {},
         gopls = {},
         html = {},
-        htmx = {},
         tailwindcss = {},
       },
     },
+    init = function()
+      -- File type detection for GDScript
+      vim.filetype.add({
+        extension = {
+          gd = "gdscript",
+        },
+        filename = {
+          ["project.godot"] = "gdresource",
+        },
+      })
+    end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
     version = "1.32.0",
+  },
+  -- Ensure GDScript treesitter parser is installed
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "gdscript" })
+    end,
   },
 }
