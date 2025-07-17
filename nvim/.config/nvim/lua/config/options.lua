405b00fecca6f69934e505465aa7aadc9af4b3ca
ParzivalVulpine <dgaitan@calicofox.dev>
2025-07-16 21:47:28 -0500

    refactor(nvim): remake lazyvim conf

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
-vim.o.scrolloff = 10
 vim.g.lazyvim_prettier_needs_config = false
+vim.o.scrolloff = 999
 vim.opt.spelllang = { "en", "es" }
 vim.o.textwidth = 88
