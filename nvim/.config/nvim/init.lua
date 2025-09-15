db1c311e5468488f9f2a6a63c838bfe1006a4f18
ParzivalVulpine <dgaitan@calicofox.dev>
2025-09-14 21:06:44 -0500

    feat(nvim): add ty lsp workaround for lazyvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.lsp.config("ty", {
+  settings = {
+    ty = {
+      experimental = {
+        rename = true,
+      },
+    },
+  },
+})
+vim.lsp.enable("ty")
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 vim.g.snacks_animate = false
 vim.g.lazyvim_prettier_needs_config = false
+vim.g.lazyvim_python_ruff = "ruff"
 vim.o.scrolloff = 999
 vim.opt.spelllang = { "en", "es" }
 vim.o.textwidth = 88
