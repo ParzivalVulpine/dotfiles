74b94f4c0cc69a22f32d53ba388b4895b5200290
ParzivalVulpine <dgaitan@calicofox.dev>
2025-09-14 20:53:17 -0500

    feat(nvim): add formatting defaults

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,6 +3,10 @@ return {
   opts = {
     formatters_by_ft = {
       html = { "prettier" },
+      htmldjango = { "prettier" },
+      javascript = { "prettier" },
+      json = { "prettier" },
+      markdown = { "prettier" },
     },
   },
 }
