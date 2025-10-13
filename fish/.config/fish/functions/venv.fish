5b4849da2eabf1dba2d1511b5bd3630110911d89
ParzivalVulpine <dgaitan@calicofox.dev>
2025-10-12 23:24:51 -0500

    fix(fish): print project folder name on venv function

--- fish/.config/fish/functions/venv.fish
+++ fish/.config/fish/functions/venv.fish
@@ -1,8 +1,8 @@
 function venv
     set venv_path (find . -maxdepth 3 -type f -path "*/bin/activate.fish" -print -quit)
     if test -n "$venv_path"
-        set dir (dirname (dirname $venv_path))
-        echo (set_color green)">Activating virtualenv at "(set_color cyan)$dir(set_color normal)
+        set project (basename (pwd))
+        echo (set_color green)">Activating virtualenv for "(set_color cyan)$project(set_color normal).
         source $venv_path
     else
         echo (set_color red)">No virtualenv found in this project"(set_color normal)
