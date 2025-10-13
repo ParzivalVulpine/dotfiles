b674970bd80f0ae4252e255615f2a531b0825009
ParzivalVulpine <dgaitan@calicofox.dev>
2025-10-12 23:13:34 -0500

    fix(fish): fix venv search depth

--- fish/.config/fish/functions/venv.fish
+++ fish/.config/fish/functions/venv.fish
@@ -1,5 +1,5 @@
 function venv
-    set venv_path (find . -maxdepth 2 -type f -path "*/bin/activate.fish" -print -quit)
+    set venv_path (find . -maxdepth 3 -type f -path "*/bin/activate.fish" -print -quit)
     if test -n "$venv_path"
         set dir (dirname (dirname $venv_path))
         echo (set_color green)">Activating virtualenv at "(set_color cyan)$dir(set_color normal)
