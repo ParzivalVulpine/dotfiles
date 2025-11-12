73b6d7a70b3780bfc26bd4f51e23fc797f4aa4b4
ParzivalVulpine <dgaitan@calicofox.dev>
2025-11-12 10:51:11 -0500

    feat(tmux): change tmux-work command to open 5 windows

--- fish/.config/fish/functions/tmux-work.fish
+++ fish/.config/fish/functions/tmux-work.fish
@@ -1,4 +1,13 @@
 function tmux-work
     set session (basename (pwd))
-    tmux new-session -s $session
+    # Create the session detached, with the first window
+    tmux new-session -d -s $session
+
+    # Create 4 more windows
+    for i in (seq 2 5)
+        tmux new-window -t $session
+    end
+
+    # Attach to the session
+    tmux attach-session -t $session
 end
